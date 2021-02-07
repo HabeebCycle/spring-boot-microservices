ea8e9655a671fb2be584640d6dead9f551200a9a
Habeeb Okunade <guessy2k2@yahoo.com>
2021-02-07 20:37:17 +1100

    Test updated

--- cloud-services/api-gateway/src/test/java/com/habeebcycle/microservices/cloud/apigateway/ApiGatewayApplicationTests.java
+++ cloud-services/api-gateway/src/test/java/com/habeebcycle/microservices/cloud/apigateway/ApiGatewayApplicationTests.java
@@ -4,42 +4,17 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.*;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.util.SocketUtils;
-
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.time.Duration;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-		properties = {"eureka.client.enabled=false"/*,"management.server.port=${test.port}"*/}
+		properties = {"eureka.client.enabled=false"}
 )
-//@AutoConfigureWebTestClient(timeout = "36000")
+@AutoConfigureWebTestClient(timeout = "36000")
 class ApiGatewayApplicationTests {
 
-	//protected static int managementPort;
 
 	/*@LocalServerPort
 	protected int port = 0;*/
-	//protected String baseUri;
-
-	@Autowired
-	protected WebTestClient client;
-
-	/*@BeforeAll
-	public static void beforeAll() {
-		managementPort = SocketUtils.findAvailableTcpPort();
-
-		System.setProperty("test.port", String.valueOf(managementPort));
-	}*/
-
-	/*@AfterAll
-	public static void afterAll() {
-		System.clearProperty("test.port");
-	}*/
 
 	/*@BeforeEach
 	void setUp() {
@@ -51,19 +26,24 @@ class ApiGatewayApplicationTests {
 				.build();
 	}*/
 
+	/*@Autowired
+	protected TestRestTemplate testRestTemplate;*/
+
+	@Autowired
+	protected WebTestClient client;
+
 	@Test
 	void contextLoads() {
 		client.get()
-				//.uri("http://localhost:" + managementPort + "/actuator/gateway/routes")
 				.uri("/actuator/gateway/routes")
 				.exchange()
 				.expectStatus()
 				.isOk();
 	}
 
-	/*@Test
-	void iFeelLuckyRouteTest() {
-		/*client.get()
+	@Test
+	void iFeelLuckyRoute200Test() {
+		client.get()
 				.uri("/headerrouting")
 				.header("Host", "i.feel.lucky:8080")
 				.exchange()
@@ -71,7 +51,7 @@ class ApiGatewayApplicationTests {
 				.expectBody(String.class)
 				.value(response -> Assertions.assertEquals("200 OK", response));
 
-		String expectedResponseBody = "200 OK";
+		/*String expectedResponseBody = "200 OK";
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Host", "i.feel.lucky:8080");
 
@@ -79,11 +59,11 @@ class ApiGatewayApplicationTests {
 		ResponseEntity<String> entity = testRestTemplate
 				.exchange("/headerrouting", HttpMethod.GET, httpEntity, String.class);
 		Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
-		Assertions.assertEquals(expectedResponseBody, entity.getBody());
-	}*/
+		Assertions.assertEquals(expectedResponseBody, entity.getBody());*/
+	}
 
-	/*@Test
-	void imATeaPotRouteTest() {
+	@Test
+	void imATeaPotRoute418Test() {
 		client.get()
 				.uri("/headerrouting")
 				.header("Host","im.a.teapot:8080")
@@ -91,6 +71,16 @@ class ApiGatewayApplicationTests {
 				.expectStatus().isEqualTo(418)
 				.expectBody(String.class)
 				.value(response -> Assertions.assertEquals("418 I'm a teapot", response));
-	}*/
+	}
+
+	@Test
+	void anyOtherRoute501Test() {
+		client.get()
+				.uri("/headerrouting")
+				.exchange()
+				.expectStatus().isEqualTo(501)
+				.expectBody(String.class)
+				.value(response -> Assertions.assertEquals("501 Not Implemented", response));
+	}
 
 }
