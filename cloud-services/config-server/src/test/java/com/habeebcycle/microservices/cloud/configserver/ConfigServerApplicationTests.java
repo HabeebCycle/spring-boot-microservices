8cc2714fc71dadc7db38b44b13bfa6f76e2816e8
Habeeb Okunade <guessy2k2@yahoo.com>
2021-04-03 12:14:01 +1100

    Cloud config server

---
+++ cloud-services/config-server/src/test/java/com/habeebcycle/microservices/cloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habeebcycle.microservices.cloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
